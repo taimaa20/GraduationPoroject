adc7d3035848e4112e5db51506659fc142d86431
taimaahamarneh <taimaahamarneh1@gmail.com>
2022-04-09 00:19:19 +0300

    s

--- WebApplication29/Models/Joins.cs
+++ WebApplication29/Models/Joins.cs
@@ -2,18 +2,19 @@
 
 namespace WebApplication29.Models
 {
-    internal class joins
+    public class joins
     {
-        public Role roles { get; set; }
-        public User users { get; set; }
-        public Login logins { get; set; }
-        public Salary salaries { get; set; }
-        public Service services { get; set; }
-        public UserService userService { get; set; }
-        public Review review { get; set; }
+        public Role? roles { get; set; }
+        public User? users { get; set; }
+        public Login? logins { get; set; }
+        public Salary? salaries { get; set; }
+        public Service? services { get; set; }
+        public UserService? userService { get; set; }
+        public Review ?review { get; set; }
 
-        public Message message { get; set; }
-        public Payment payment { get; set; }
+        public Message ?message { get; set; }
+        public Payment ? payment { get; set; }
+       
 
     }
 }
